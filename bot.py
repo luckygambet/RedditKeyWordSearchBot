53bb7a84243cec144782204bf3bb92e44dc794ad
luckygambet <stolen124@gmail.com>
2021-02-17 23:59:20 -1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -83,7 +83,7 @@ class word_search():
 
 def BuildMessage(word_searchArray):
     """
-        if LINKS_IN_MESSAGE is comments and links
+        if LINKS_IN_MESSAGE True it links
         will be included in the message
         RETURNS ARRAY OF STRINGS
 
